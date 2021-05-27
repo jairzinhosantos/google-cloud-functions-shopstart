5657fc6c8b654431cdd98ab882a4149f81b62889
Jairzinho <jairzinho.santos.fajardo@gmail.com>
2021-05-27 12:04:01 -0500

    create cloud functions and deploy

---
+++ helloworld/main.py
@@ -0,0 +1,21 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def hello_world(request):
+    request_args = request.args
+    request_json = request.get_json(silent=True)
+    if request_args and 'name' in request_args and 'lastname' in request_args:
+        name = request_args['name']
+        lastname = request_args['lastname']
+    elif request_json and 'name' in request_json and request_json and 'lastname' in request_json:
+        name = request_json['name']
+        lastname = request_args['lastname']
+    else:
+        name = 'World'
+        lastname = ''
+    return f'Hello {name} {lastname}'
+
+
